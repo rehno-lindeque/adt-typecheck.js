e4bc2acb40389ae4cb43b14dab3d7a7acd959ced
Rehno Lindeque <errantkid@gmail.com>
2012-05-28 22:35:42 -0700

    Implement very simple type checking

--- dist/adt-typecheck.js
+++ dist/adt-typecheck.js
@@ -7,8 +7,59 @@
 var adt = adt || {};
 (function() {
 "use strict";
-  adt.typecheck = function(obj){
-    // TODO
+  // TODO: put in an adt-json library
+  adt.JSON = {};
+  adt.JSON.tags = [
+    'Number',
+    'String',
+    'Boolean',
+    'Array',
+    'Object',
+    'Null'
+  ];
+  adt.JSON.constructors = adt.apply(null, adt.JSON.tags);
+
+  // TODO: put in an adt-class library
+  adt.Class = {};
+  adt.Class.tags = [
+    'Arguments',
+    'Array',
+    'Boolean',
+    'Date',
+    'Error',
+    'Function',
+    'JSON',
+    'Math',
+    'Number',
+    'Object',
+    'RegExp',
+    'String',
+    'Null',
+    'Undefined'
+  ];
+  adt.Class.constructors = adt.apply(null, adt.Class.tags);
+
+  // adt-typecheck.js
+  var
+    typeError = function(expected, received, key) {
+      if (key)
+        return { key: key, expected: expected, received: received };
+      else
+        return { expected: expected, received: received };
+    };
+
+  adt.typecheck = function(schemaF) {
+    var schema = schemaF.call(adt.Class.constructors);
+    return adt({
+      _: function(){
+        var
+          expected = this._tag,
+          evals = {};
+        evals[expected] = [];
+        evals['_'] = function(){ return [typeError(expected, this._tag)]; };
+        return adt(evals);
+      }
+    })(schema);
   };
   // Export typecheck to a CommonJS module if exports is available
   if (typeof module !== "undefined" && module !== null)
